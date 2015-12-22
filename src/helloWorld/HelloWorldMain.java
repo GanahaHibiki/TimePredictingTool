dfd500a51012ce7f37e9ea53e135e7ce884198c3
GanahaHibiki <toshilang@hotmail.com>
2015-12-22 23:59:35 +0800

    Create time predicting tool project

---
+++ src/helloWorld/HelloWorldMain.java
@@ -0,0 +1,15 @@
+package helloWorld;
+
+import javax.swing.JFrame;
+
+
+
+public class HelloWorldMain extends JFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
